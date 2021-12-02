2fda16d0fbde2a28c950673ed6f7ce66f40db203
Bram in 't Zandt <bram@bigspark.com>
2021-12-02 06:35:27 +0100

    ⌚ Run every minute instead of every four

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new TryToRegister)->everyFourMinutes();
+        $schedule->job(new TryToRegister)->everyMinute();
     }
 
     /**
